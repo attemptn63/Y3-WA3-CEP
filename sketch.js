644fcde825ef8c42802e71cee260b1f89754f287
attemptn63 <93580500+attemptn63@users.noreply.github.com>
2023-07-11 20:35:42 +0800

    Animation Stage 1
    - animates the final hull
    - need to work on trying to animate indexing, then work on the entire process

--- sketch.js
+++ sketch.js
@@ -50,33 +50,45 @@ function draw(){
     resetbutton.draw();
     convexhullbutton.draw();
     if(mode == "hull" && hull_loop < hull.length - 1){
-        if(percentage >= 1.029){
+        if(percentage >= 1.05){
             hull_loop++;
+            if(hull_loop == hull.length - 1){
+                percentage = 0;
+                return;
+            }
             percentage = 0;
         }
         line_start = hull[hull_loop].copy();
         line_end = hull[hull_loop + 1].copy();
         drawHull(hull_loop, percentage);
-        percentage += 0.03;
+        percentage += 0.05;
+    }
+    for(var i = 0; i < points.length; i++){
+        circle(points[i].x,points[i].y,60);
+        textSize(32);
+        textAlign(CENTER,CENTER);
+        if(mode == "hull"){
+            text(i+1,points[i].x,points[i].y);
+        }
     }
 }
 
 function mousePressed(){
     if(mode == "edit" && (mouseX > 400 || mouseY > 100)){
-        circle(mouseX,mouseY,60);
         points.push(createVector(mouseX,mouseY));
     }
 }
-function drawHull(i, percentage){
+function drawHull(hull_loop, percentage){
     var dist = line_start.dist(line_end);
-    var start = hull[i].copy();
-    start.lerp(line_end, 31/dist);
-    var end = hull[i+1].copy();
-    end.lerp(line_start, 31/dist);
-    start.lerp(end, percentage);
+    var start = hull[hull_loop].copy();
+    start.lerp(line_end.copy(), 31/dist);
+    var anim_end = start.copy();
+    var end = hull[hull_loop+1].copy();
+    end.lerp(line_start.copy(), 31/dist);
+    anim_end.lerp(end, percentage);
     push();
     strokeCap(SQUARE);
     strokeWeight(5);
-    line(start.x,start.y,end.x,end.y);
+    line(start.x,start.y,anim_end.x,anim_end.y);
     pop();
 }
