3c70d67affb3a2be9ce9309ded178cf7f6dcf235
attemptn63 <93580500+attemptn63@users.noreply.github.com>
2023-07-13 18:38:46 +0800

    Animating indexing and hull drawing
    Changes:
    - when indexing, the point index is only drawn after the line reaches the point
    - when drawing convex hull, the lines are yellow and the points become yellow if they are part of the hull
    - changed how I treat the hull[] array, instead of storing vectors, stores the indices of the points array

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 var editbutton,mode,cancelbutton,resetbutton,convexhullbutton;
 var points = [],hull = [];
 var startframe = 0, endframe = 0;
-var hull_loop = 0, percentage = 0;
-var line_start, line_end;
+var i_loop = 0, percentage = 0;
+var line_start, line_end, undraw;
 function setup(){
     createCanvas(windowWidth, windowHeight);
     background(255);
@@ -24,19 +24,23 @@ function createButtons(){
         mode = "edit";
     }
     cancelbutton.onPress = function(){
-        mode = "view";
+        if(mode == "edit"){
+            mode = "view";
+        }
     }
     resetbutton.onPress = function(){
         background(255);
         points = [];
         hull = [];
-        hull_loop = 0;
+        i_loop = 0;
         percentage = 0;
+        undraw = 0;
     }
     convexhullbutton.onPress = function(){
-        if(mode != "hull" && points.length > 2){
+        if(mode != "hull" && mode != "index" && points.length > 2){
             hull = convexhull(points);
-            mode = "hull";
+            mode = "index";
+            i_loop = 1;
         }
     }
     editbutton.text = "Edit";
@@ -49,44 +53,95 @@ function draw(){
     cancelbutton.draw();
     resetbutton.draw();
     convexhullbutton.draw();
-    if(mode == "hull" && hull_loop < hull.length - 1){
+    if(mode == "index"){
         if(percentage >= 1.05){
-            hull_loop++;
-            if(hull_loop == hull.length - 1){
+            if(undraw && i_loop == points.length - 1){
+                mode = "hull";
+                percentage = 0;
+                i_loop = 0;
+                return;
+            }
+            if(undraw == 1){
+                undraw = 0;
+                i_loop++;
+            }
+            else{
+                undraw = 1;
+            }
+            percentage = 0;
+        }
+        else if(undraw){
+            line_start = points[i_loop].copy();
+            line_end = points[0].copy();
+            drawLine(percentage, "white", 6);
+        }
+        else{
+            line_start = points[0].copy();
+            line_end = points[i_loop].copy();
+            drawLine(percentage, "black");
+        }
+        percentage += 0.05;
+        for(var i = 0; i < points.length; i++){
+            circle(points[i].x,points[i].y,60);
+            if(i < i_loop || (i == i_loop && undraw)){
+                textSize(32);
+                textAlign(CENTER,CENTER);
+                text(i,points[i].x,points[i].y);
+            }
+        }
+    }
+    if(mode == "hull" && i_loop < hull.length - 1){
+        if(percentage >= 1.05){
+            i_loop++;
+            if(i_loop == hull.length - 1){
                 percentage = 0;
                 return;
             }
             percentage = 0;
         }
-        line_start = hull[hull_loop].copy();
-        line_end = hull[hull_loop + 1].copy();
-        drawHull(hull_loop, percentage);
+        line_start = points[hull[i_loop]].copy();
+        line_end = points[hull[i_loop+1]].copy();
+        drawLine(percentage, "#ce9102");
         percentage += 0.05;
-    }
-    for(var i = 0; i < points.length; i++){
-        circle(points[i].x,points[i].y,60);
-        textSize(32);
-        textAlign(CENTER,CENTER);
-        if(mode == "hull"){
+        for(var i = 0; i < points.length; i++){
+            circle(points[i].x,points[i].y,60);
+            textSize(32);
+            textAlign(CENTER,CENTER);
             text(i,points[i].x,points[i].y);
+        }
+        for(var i = 0; i <= i_loop; i++){
+            push();
+                stroke("#ce9102");
+                noFill();
+                circle(points[hull[i]].x,points[hull[i]].y,60);
+                fill("#ce9102");
+                textSize(32);
+                textAlign(CENTER,CENTER);
+                text(hull[i],points[hull[i]].x,points[hull[i]].y);
+            pop();
+        }
+    }
+    if(mode == "edit" || mode == "view"){
+        for(var i = 0; i < points.length; i++){
+            circle(points[i].x,points[i].y,60);
         }
     }
 }
 
 function mousePressed(){
-    if(mode == "edit" && (mouseX > 400 || mouseY > 100)){
+    if(mode == "edit" && (mouseX > 400 || mouseY > 100) && (mouseX < width - 30 && mouseX > 30) && (mouseY < height - 30 && mouseY > 30)){
         points.push(createVector(mouseX,mouseY));
     }
 }
-function drawHull(hull_loop, percentage){
-    var dist = line_start.dist(line_end);
+function drawLine(percentage, color, strokeSize = 5){
     var start = line_start.copy();
     var anim_end = start.copy();
     var end = line_end.copy();
     anim_end.lerp(end, percentage);
     push();
+    stroke(color);
     strokeCap(SQUARE);
-    strokeWeight(5);
+    strokeWeight(strokeSize);
     line(start.x,start.y,anim_end.x,anim_end.y);
     pop();
 }
