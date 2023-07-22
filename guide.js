94b5b7876cbd95a55bcefef247224d14669dd096
attemptn63 <93580500+attemptn63@users.noreply.github.com>
2023-07-22 19:05:03 +0800

    slight ui change
    added box to guide
    button change

--- guide.js
+++ guide.js
@@ -6,12 +6,17 @@ function guide(){
     nextbutton.draw();
     if(slide == 0){
         push();
+        rect(90,250,1250,300,20);
         textSize(20);
         textAlign(LEFT,CENTER);
         text(simul_info,100,100,windowWidth-150,windowHeight-150);
         pop();
     }
     else if(slide == 1){
+        push();
+        rectMode(CENTER);
+        rect(windowWidth/2 - 30,windowHeight/2-50,550,300,20)
+        pop();
         push();
         textSize(20);
         textAlign(CENTER,CENTER);
